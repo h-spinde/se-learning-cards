e5462cc461be7519e88b780a3962d4a3afd7f677
h-spinde <hannah.spinde@st.ovgu.de>
2023-06-30 20:43:47 +0200

    removing subclasses from LearningCard.java file to make public

---
+++ com/cards/QuestionCard.java
@@ -0,0 +1,54 @@
+package cards;
+
+import java.util.List;
+import java.util.*;
+
+// ------------------------------------------------------ //
+public class QuestionCard extends LearningCard {
+
+  private String title;
+  private String front;
+  private String back;
+  private int counter = 0;
+    
+  public QuestionCard(String ttl, String question, String answer) {
+    this.title = removeEmptyLine(ttl);
+    this.front = removeEmptyLine(question);
+    this.back = removeEmptyLine(answer);
+    this.counter = 0;
+  }
+    
+  public QuestionCard(String ttl, String question, String answer, int count) {
+    this.title = removeEmptyLine(ttl);
+    this.front = removeEmptyLine(question);
+    this.back = removeEmptyLine(answer);
+    this.counter = count;
+  }
+    
+  public List<String> getFrontContent() {
+    List<String> myList = new ArrayList<>();
+    myList.add(this.title);
+    myList.add(this.front);
+    return myList;
+  }
+    
+  public List<String> getBackContent() {
+    List<String> myList = new ArrayList<>();
+    myList.add(this.back);
+    return myList;
+  }
+    
+  public List<String> getContent() {
+    List<String> myList = new ArrayList<>();
+    myList.add(this.title);
+    myList.add(this.front);
+    myList.add(this.back);
+    return myList;
+  }
+    
+  public void printToConsole() {
+    System.out.println("the title of this card is: " + this.title);
+    System.out.println("the front matter is: " + this.front);
+    System.out.println("the back matter is: " + this.back);
+  }
+}
---
+++ com/cards/SimpleCard.java
@@ -0,0 +1,49 @@
+package cards;
+
+import java.util.List;
+import java.util.*;
+
+// ------------------------------------------------------ //
+public class SimpleCard extends LearningCard {
+
+  private String front;
+  private String back;
+  private int counter;
+    
+  public SimpleCard(String question, String answer) {
+    this.front = removeEmptyLine(question);
+    this.back = removeEmptyLine(answer);
+    this.counter = 0;
+  }
+    
+  public SimpleCard(String question, String answer, int count) {
+    this.front = removeEmptyLine(question);
+    this.back = removeEmptyLine(answer);
+    this.counter = count;
+  }
+    
+  public List<String> getFrontContent() {
+    List<String> myList = new ArrayList<>();
+    myList.add(this.front);
+    return myList;
+  }
+    
+  public List<String> getBackContent() {
+    List<String> myList = new ArrayList<>();
+    myList.add(this.back);
+    return myList;
+  }
+    
+  public List<String> getContent() {
+    List<String> myList = new ArrayList<>();
+    myList.add(this.front);
+    myList.add(this.back);
+    return myList;
+  }
+    
+  public void printToConsole() {
+    System.out.println("the front matter is: " + this.front);
+    System.out.println("the back matter is: " + this.back);
+  }
+  
+}
